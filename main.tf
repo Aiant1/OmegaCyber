331cb9592352ae940a062703c69f4d08b32f3500
Antikadas1 <antika.das95@gmail.com>
2026-01-16 19:08:21 +0100

    Initial commit

--- main.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-required_providers {
-    aws   = { source = "hashicorp/aws",   version = "~> 5.0" }
-    azurerm = { source = "hashicorp/azurerm", version = "~> 4.0" }
-    google = { source = "hashicorp/google", version = "~> 5.0" }
-}
-}
